2c605674b5983f708a837155473bc488a47fe92b
Matthew Ragan <raganmd@gmail.com>
2025-03-11 17:19:21 -0700

    Starting point for Icon Template component

--- scripts/project_settings.py
+++ scripts/project_settings.py
@@ -1,4 +1,4 @@
-TD_VERSION: str = "2023.12120"
+TD_VERSION: str = "2023.12230"
 PROJECT_FILE: str = "./TouchDesigner/project.toe"
-REPO: str = "https://github.com/SudoMagicCode/td-tox-builder-template"
-COMP_NAME: str = "Empty"
+REPO: str = "https://github.com/SudoMagicCode/td-comp-icon"
+COMP_NAME: str = "base_icon_ui"
